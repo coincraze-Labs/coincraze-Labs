397d6e479ec6af0e8dbb5af84afe3d428c8e3cef
Your Name <you@example.com>
2024-09-08 09:14:40 +0800

    set price

--- assets/typeScript/ui/popupBuyItem.ts
+++ assets/typeScript/ui/popupBuyItem.ts
@@ -41,15 +41,17 @@ export class popupBuyItem extends Component {
 
     private _connectUI: TonConnectUI;
     private client: TonClient;
+    private _bnt1Num:number = 0;
+    private _bnt2Num:number = 0;
 
-    private RECIVER:string = "UQBfpRJFEr3__8uIvfXnkxWPf1cOsT8UwaM-mprM7XjFD6Vj"
+    private RECEIVER:string = "UQBfpRJFEr3__8uIvfXnkxWPf1cOsT8UwaM-mprM7XjFD6Vj"
 
 
     protected start(): void {
         
         EventManger.eventTarget.on(EventManger.EEventName.REFRESH_GAME, this.refresh, this);
     }
-    
+
     onEnable() {
         HttpClient.getInstance().sendTonPrice();
         this.refresh()
@@ -58,7 +60,9 @@ export class popupBuyItem extends Component {
     refresh(){
         
         this._cocosGameFi = gameData.cocosGameFi;
-        this._connectUI =this._cocosGameFi.walletConnector;  
+        if(this._cocosGameFi){
+            this._connectUI =this._cocosGameFi.walletConnector;  
+        }
         this.itemData = gameData.curBuyShopData;
         this.buyNum = gameData.curBuyCount;
         this.client = new TonClient({
@@ -69,16 +73,19 @@ export class popupBuyItem extends Component {
         }
 
         let coin = this.itemData.coin_num * this.buyNum;
+        this._bnt1Num = coin*0.9*gameData.saveData.tonPrice;
+        this._bnt2Num = coin*0.9;
 
         this.titllab1.string = LanauageManager.getDesStrById(65);
 
         this.btn3_lab1.string = LanauageManager.getDesStrById(gameData.isBindWallet ? 63: 62 )
 
-        this.btn1_lab1.string = LanauageManager.getCoinNumString(coin*0.9*gameData.saveData.tonPrice) + " " + LanauageManager.getDesStrById(105);
+        this.btn1_lab1.string = LanauageManager.getCoinNumString(this._bnt1Num) + " " + LanauageManager.getDesStrById(105);
+        console.log(this.btn1_lab1.string)
         this.btn1_lab2.string = LanauageManager.getCoinNumString(coin)  + LanauageManager.getDesStrById(105);
         //this.btn1_lab3.string = "10%" + LanauageManager.getDesStrById(107);
 
-        this.btn2_lab1.string = LanauageManager.getCoinNumString(coin*0.9) + " " +  LanauageManager.getDesStrById(106);
+        this.btn2_lab1.string = LanauageManager.getCoinNumString(this._bnt2Num) + " " +  LanauageManager.getDesStrById(106);
         this.btn2_lab2.string = LanauageManager.getCoinNumString(coin)  +  LanauageManager.getDesStrById(106);
         //this.btn2_lab3.string = "10%" + LanauageManager.getDesStrById(107);
 
@@ -116,8 +123,8 @@ export class popupBuyItem extends Component {
             validUntil:Math.floor(Date.now() / 1000) + 360,
             messages: [
                 {
-                    address: this.RECIVER, 
-                    amount: toNano(0.1).toString()
+                    address: this.RECEIVER, 
+                    amount: toNano(this._bnt1Num).toString()
                 }
             ]
         }
@@ -134,9 +141,9 @@ export class popupBuyItem extends Component {
             const body = beginCell()
             .storeUint(0xf8a7ea5, 32)     
             .storeUint(0, 64)           
-            .storeCoins(toNano("0.1"))  
-            .storeAddress(Address.parse(this.RECIVER)) 
-            .storeAddress(Address.parse(this.RECIVER)) 
+            .storeCoins(toNano(this._bnt2Num/1000))  
+            .storeAddress(Address.parse(this.RECEIVER)) 
+            .storeAddress(Address.parse(this.RECEIVER)) 
             .storeMaybeRef(null)        
             .storeCoins(1)
             .storeMaybeRef(null)       
