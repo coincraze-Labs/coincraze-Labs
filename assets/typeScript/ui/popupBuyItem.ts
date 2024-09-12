6e92532d4f3884a51f1501d77d52cc70900865ac
metalucky <116775636+jasonq11@users.noreply.github.com>
2024-09-12 11:07:54 +0800

    Update popupBuyItem.ts

--- assets/typeScript/ui/popupBuyItem.ts
+++ assets/typeScript/ui/popupBuyItem.ts
@@ -4,6 +4,7 @@ import { LanauageManager, ShopItemData } from '../LanauageManager';
 import { gameData, popupCommonType, popupLabType } from '../gameData';
 import { EventManger } from '../EventManger';
 import { HttpClient } from '../net/HttpClient';
+import { GameFi,beginCell, Cell,TonConnectUI,SendTransactionRequest, Address , toNano ,TonClient } from '@ton/cocos-sdk';
 const { ccclass, property } = _decorator;
 
 @ccclass('popupBuyItem')
@@ -37,6 +38,16 @@ export class popupBuyItem extends Component {
 
     private buyNum:number = 0;
 
+    private _cocosGameFi: GameFi;
+
+    private _connectUI: TonConnectUI;
+    private client: TonClient;
+    
+    private _bnt1Num:number = 0;
+    private _bnt2Num:number = 0;
+
+    private RECEIVER:string = "UQBfpRJFEr3__8uIvfXnkxWPf1cOsT8UwaM-mprM7XjFD6Vj"
+
     protected start(): void {
         EventManger.eventTarget.on(EventManger.EEventName.REFRESH_GAME, this.refresh, this);
     }
@@ -47,6 +58,15 @@ export class popupBuyItem extends Component {
     }
 
     refresh(){
+           this._cocosGameFi = gameData.cocosGameFi;
+        if(this._cocosGameFi){
+            this._connectUI =this._cocosGameFi.walletConnector;  
+        }
+        this.itemData = gameData.curBuyShopData;
+        this.buyNum = gameData.curBuyCount;
+        this.client = new TonClient({
+            endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+        });
         this.itemData = gameData.curBuyShopData;
         this.buyNum = gameData.curBuyCount;
         if (!this.itemData){
@@ -58,7 +78,9 @@ export class popupBuyItem extends Component {
         this.titllab1.string = LanauageManager.getDesStrById(65);
 
         this.btn3_lab1.string = LanauageManager.getDesStrById(gameData.isBindWallet ? 63: 62 )
-
+        
+        this._bnt1Num = coin*0.9*gameData.saveData.tonPrice;
+        this._bnt2Num = coin*0.9;
         this.btn1_lab1.string = LanauageManager.getCoinNumString(coin*0.9/gameData.saveData.tonPrice) + " " + LanauageManager.getDesStrById(105);
         this.btn1_lab2.string = LanauageManager.getCoinNumString(coin/gameData.saveData.tonPrice)  + LanauageManager.getDesStrById(105);
         //this.btn1_lab3.string = "10%" + LanauageManager.getDesStrById(107);
@@ -78,6 +100,7 @@ export class popupBuyItem extends Component {
         LanauageManager.playSound();
         if (gameData.isBindWallet){
             let coin = Math.floor(this.itemData.coin_num * this.buyNum*0.9/gameData.saveData.tonPrice);
+            this.onTransferTon()
             EventManger.eventTarget.emit(EventManger.EEventName.OPEN_TON_CONNNECT, 2, coin);
 
         }else{
@@ -90,6 +113,7 @@ export class popupBuyItem extends Component {
         if (gameData.isBindWallet){
             let coin = Math.floor(this.itemData.coin_num * this.buyNum*0.9);
             EventManger.eventTarget.emit(EventManger.EEventName.OPEN_TON_CONNNECT, 2, coin);
+             this.onTransferJetton()
         }else{
             EventManger.eventTarget.emit(EventManger.EEventName.OPEN_TON_CONNNECT);
         }
@@ -102,6 +126,61 @@ export class popupBuyItem extends Component {
 
         HttpClient.getInstance().sendGetinvoiceLink(LanauageManager.getDesStrById(this.itemData.name_id), coin)
     }
+  async  getUserWalletAddress(userAddress, jettonMasterAddress) {
+       
+        const userAddressCell = beginCell().storeAddress(userAddress).endCell()
+        const response = await this.client.runMethod(jettonMasterAddress, "get_wallet_address", [
+            {type: "slice", cell: userAddressCell}
+        ])
+        return response.stack.readAddress()
+    } 
+    async onTransferTon() {
+        if (this._cocosGameFi && this._cocosGameFi.walletConnector?.connected) {
+        const transaction = {
+            validUntil:Math.floor(Date.now() / 1000) + 360,
+            messages: [
+                {
+                    address: this.RECEIVER, 
+                    amount: toNano(this._bnt1Num).toString()
+                }
+            ]
+        }
+        const result = await this._connectUI.sendTransaction(transaction as SendTransactionRequest)  
+        const hash = Cell.fromBase64(result.boc)
+        .hash()
+        .toString("base64");
 
+    }
+    }
+    async  onTransferJetton() {
+        if (this._cocosGameFi && this._cocosGameFi.walletConnector?.connected) {
+            const userAddress = Address.parse(this._connectUI.account.address)
+            const body = beginCell()
+            .storeUint(0xf8a7ea5, 32)     
+            .storeUint(0, 64)           
+            .storeCoins(toNano(this._bnt2Num/1000))  
+            .storeAddress(Address.parse(this.RECEIVER)) 
+            .storeAddress(Address.parse(this.RECEIVER)) 
+            .storeMaybeRef(null)        
+            .storeCoins(1)
+            .storeMaybeRef(null)       
+            .endCell(); 
+            const jettonMasterAddress = Address.parse("EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs")
+            const jettonWalletAddress = await this.getUserWalletAddress(userAddress, jettonMasterAddress)
+            
+        const transaction = {
+            validUntil: Math.floor(Date.now() / 1000) + 360,
+            messages: [
+                {
+                    address: jettonWalletAddress.toString(), 
+                    amount: toNano("0.1").toString(), 
+                    payload: body.toBoc().toString("base64") 
+                }
+            ]
+        }
+        const result = await this._connectUI.sendTransaction(transaction as SendTransactionRequest)
+        console.log(`Send tx result: ${JSON.stringify(result)}`);
+        }
+    }
 }
 
